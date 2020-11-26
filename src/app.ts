af4164c44c4ebf825e8c26d055d62d87aab4864c
glowies <oktaycomu@gmail.com>
2020-11-26 18:33:04 -0500

    Add sphere based drawing.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
-import { ButtonBehavior } from '@microsoft/mixed-reality-extension-sdk';
 
 /**
  * The main class of this app. All the logic goes here.
  */
-export default class HelloWorld {
-	private plane: MRE.Actor = null;
+export default class Whiteboard {
 	private assets: MRE.AssetContainer;
+	private drawSurface: MRE.Actor = null;
+	private drawMesh: MRE.Mesh;
+	private hoverMaterial: MRE.Material;
+	private drawObjects: MRE.Actor[] = [];
 
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
@@ -24,7 +21,12 @@ export default class HelloWorld {
 		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
 		this.assets = new MRE.AssetContainer(this.context);
 
-		this.plane = MRE.Actor.CreatePrimitive(this.assets, {
+		this.drawMesh = this.assets.createSphereMesh('drawPoint', .01);
+		this.hoverMaterial = this.assets.createMaterial('hoverMaterial', {
+			color: MRE.Color3.Gray()
+		});
+		
+		this.drawSurface = MRE.Actor.CreatePrimitive(this.assets, {
 			actor: {
 				transform: {
 					local: { 
@@ -33,15 +35,41 @@ export default class HelloWorld {
 						scale: {x: 3, y: 1, z: 2}
 					}
 				},
+				collider: { geometry: { shape: MRE.ColliderType.Auto} }
 			},
 			definition: {shape: MRE.PrimitiveShape.Plane}
 		});
 
-		const buttonBehavior = this.plane.setBehavior(MRE.ButtonBehavior);
+		const buttonBehavior = this.drawSurface.setBehavior(MRE.ButtonBehavior);
 
-		buttonBehavior.onButton("holding", (user: MRE.User, event: MRE.ButtonEventData) => {
-			
+		buttonBehavior.onButton("holding", (user: MRE.User, data: MRE.ButtonEventData) => {
+			this.spawnTargetObjects("draw", data.targetedPoints.map(pt => pt.localSpacePoint));
 		});
+	}
+
+	private spawnTargetObjects(targetingState: 'hover' | 'draw', drawPoints: MRE.Vector3Like[]) {
+		const materialId = this.hoverMaterial.id;
+
+		const drawActors = drawPoints.map(drawPoint => {
+			return MRE.Actor.Create(this.context, {
+				actor: {
+					name: targetingState === 'hover' ? 'hoverBall' : 'drawBall',
+					parentId: this.drawSurface.id,
+					transform: { local: { position: drawPoint } },
+					appearance: {
+						materialId: materialId,
+						meshId: this.drawMesh.id
+					}
+				}
+			});
+		});
+
+		if (targetingState === 'hover') {
+			// Set lifetime timer for the hover points.
+			setTimeout(() => drawActors.forEach(actor => actor.destroy()), 1500);
+		} else {
+			this.drawObjects = this.drawObjects.concat(drawActors);
+		}
 	}
 }
 
